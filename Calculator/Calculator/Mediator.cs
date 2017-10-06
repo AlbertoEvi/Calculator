6d4f1175827700e07cd88875f5a64bca687e713b
Alberto Gutierrez <agutierrez@evicertia.com>
2017-10-06 14:18:39 +0200

    connecting client with server

--- Calculator/Calculator/Mediator.cs
+++ Calculator/Calculator/Mediator.cs
@@ -6,20 +6,34 @@ using System.Threading.Tasks;
 using Calculator.Models;
 using System.Net;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace Calculator
 {
     public class Mediator
     {
         public static string url = "http://localhost:51186/Calculator/";
+        public static string sum;
         #region add
         public void Add(string trackingId)
         {
-            AddRequest addition = new AddRequest();
-            AddResponse result = new AddResponse();
+            Console.WriteLine("------Add Operation------");
+            Console.WriteLine("Type the sum you want to do(ex:13 + 12 + 5): ");
 
-            int[] numbers = new int[] {};
+            char symb = '+';
+            sum = Console.ReadLine();
+            string[] nums = sum.Split(symb);
+
+            int[] numbers = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                numbers[i] = int.Parse(nums[i].Trim());
+                Console.WriteLine(numbers[i]);
+            }
             
+            AddRequest addition = new AddRequest(); 
+            AddResponse result = new AddResponse();
+                        
             addition.Added = numbers;
 
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create($"{url}Add");
@@ -28,8 +42,18 @@ namespace Calculator
             
             using (StreamWriter dataStream = new StreamWriter(request.GetRequestStream()))
             {
-                dataStream.Write(addition.Added);
+                var jason = JsonConvert.SerializeObject(addition);
+                dataStream.Write(jason);
                 dataStream.Close();
+            }
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            using (StreamReader stRead = new StreamReader(response.GetResponseStream()))
+            {
+                Console.WriteLine("The server operation's result is:");
+                result = JsonConvert.DeserializeObject<AddResponse>(stRead.ReadToEnd());
+                Console.WriteLine(result.Sum);
+                stRead.Close();
             }
         }
         #endregion
--- Calculator/CalculatorService/Controllers/CalculatorController.cs
+++ Calculator/CalculatorService/Controllers/CalculatorController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using CalculatorService.Models;
+using Newtonsoft.Json;
 
 namespace CalculatorService.Controllers
 {
@@ -14,10 +15,17 @@ namespace CalculatorService.Controllers
         {
             return View();
         }
-        public void Add(AddRequest petition)
+        public string Add(AddRequest petition)
         {
             int[] nums = petition.Added;
-
+            AddResponse result = new AddResponse();
+            result.Sum = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                result.Sum += nums[i];
+            }
+            var hasonServer = JsonConvert.SerializeObject(result);
+            return hasonServer;
         }
     }
 }
